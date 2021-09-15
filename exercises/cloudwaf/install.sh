78383668495bfbb3b3307271c7c5641b85354bb6
SVMal <19540015+sam422@users.noreply.github.com>
2021-09-15 19:21:09 +1000

    nginx

--- exercises/cloudwaf/install.sh
+++ exercises/cloudwaf/install.sh
@@ -44,6 +44,7 @@ server {
   server_name jenkins.example.com;
 
   location / {
+    proxy_pass         http://127.0.0.1:8080/;
     proxy_set_header        Host $host:$server_port;
     proxy_set_header        X-Real-IP $remote_addr;
     proxy_set_header        X-Forwarded-For $proxy_add_x_forwarded_for;
