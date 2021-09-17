6b6d4fbac6a453cdaa03b023775a121212482442
SVMal <19540015+sam422@users.noreply.github.com>
2021-09-17 20:12:26 +1000

    install changes

--- exercises/jenkins-nginx/install.sh
+++ exercises/jenkins-nginx/install.sh
@@ -1,3 +1,4 @@
+# This is to install nodejs and nginx container. nginx as reverse proxy
 #!/bin/bash
 sudo yum -y update
 
@@ -11,53 +12,16 @@ sudo yum install -y maven
 echo "Install git"
 sudo yum install -y git
 
-echo "Install Docker engine"
-sudo yum update -y
-sudo amazon-linux-extras install docker
-sudo yum install docker -y
-sudo service docker start
-sudo usermod -a -G docker ec2-user
-sudo chkconfig docker on
-docker network create nginx
-docker network create localhost --internal
-
-echo "Install nodejs"
-mkdir nodejs && cd nodejs
-cat > Dockerfile <<EOF
-FROM jenkins/jenkins:lts
-LABEL maintainer=”sk@gmail.com”
-ENV JAVA_OPTS="-Xmx1024m"
-ENV JENKINS_OPTS=" --handlerCountMax=300"
-USER jenkins
-EXPOSE 8080
-EOF
-docker build -t jenkins:1.0 .
-docker run -d -p 8080:8080  jenkins:1.0 
+echo "Install Jenkins"
+amazon-linux-extras install epel -y
+sudo wget -O /etc/yum.repos.d/jenkins.repo   https://pkg.jenkins.io/redhat-stable/jenkins.repo
+sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
+sudo yum install jenkins java-1.8.0-openjdk-devel -y
+sudo systemctl daemon-reload
+sudo systemctl status jenkins
 
 echo "Install NGINX"
-mkdir nginx && cd nginx
-cat > nginx.conf <<EOF
-upstream jenkins {
-  server        127.0.0.1:8080;
-}
-
-server {
-  listen        80;
-  server_name   localhost;
-
-  location / {
-    proxy_pass  http://127.0.0.1;
-  }
-}
-EOF
-echo "Create nginx docker file and run nginx docker"
-cat > Dockerfile <<EOF
-FROM nginx
-COPY nginx.conf /etc/nginx/nginx.conf
-EOF
-docker build -t nginx:1.0 .
-docker run -p 80:8080 nginx:1.0
-
-echo "Start Docker & Jenkins services"
-sudo service docker start
-sudo service jenkins start
+sudo amazon-linux-extras install nginx1
+sudo service nginx start
+sudo chkconfig nginx on
+sudo service nginx status
