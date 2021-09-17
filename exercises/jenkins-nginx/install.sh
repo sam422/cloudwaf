2149f28cc00767f515d2ad50772270dc0544f72a
SVMal <19540015+sam422@users.noreply.github.com>
2021-09-17 22:00:57 +1000

    nginx

--- exercises/jenkins-nginx/install.sh
+++ exercises/jenkins-nginx/install.sh
@@ -13,15 +13,19 @@ echo "Install git"
 sudo yum install -y git
 
 echo "Install Jenkins"
-amazon-linux-extras install epel -y
+sudo amazon-linux-extras install epel -y
 sudo wget -O /etc/yum.repos.d/jenkins.repo   https://pkg.jenkins.io/redhat-stable/jenkins.repo
 sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
 sudo yum install jenkins java-1.8.0-openjdk-devel -y
 sudo systemctl daemon-reload
-sudo systemctl status jenkins
+sudo systemctl start jenkins
+
 
 echo "Install NGINX"
 sudo amazon-linux-extras install nginx1
-sudo service nginx start
 sudo chkconfig nginx on
 sudo service nginx status
+
+echo "Start Jenkins & nginx"
+sudo systemctl start jenkins
+sudo service nginx start
