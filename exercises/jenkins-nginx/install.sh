2c858125c552733be56058f39acfb37ac47c5d16
SVMal <19540015+sam422@users.noreply.github.com>
2021-09-18 12:57:29 +1000

    Update install.sh

--- exercises/jenkins-nginx/install.sh
+++ exercises/jenkins-nginx/install.sh
@@ -1,4 +1,3 @@
-# This is to install nodejs and nginx container. nginx as reverse proxy
 #!/bin/bash
 sudo yum -y update
 
@@ -18,7 +17,6 @@ sudo wget -O /etc/yum.repos.d/jenkins.repo   https://pkg.jenkins.io/redhat-stabl
 sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
 sudo yum install jenkins java-1.8.0-openjdk-devel -y
 sudo systemctl daemon-reload
-sudo systemctl start jenkins
 
 
 echo "Install NGINX"
